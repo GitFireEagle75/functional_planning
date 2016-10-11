24dbdc09c6204d06d71eacc9f1329e825abf0842
GitFireEagle75 <marcelfeesche@googlemail.com>
2016-10-11 07:53:24 +0200

    fehlerbehebung inner multidim Array

--- pages/Fahrzeug_Trupp_Bezeichnung.php
+++ pages/Fahrzeug_Trupp_Bezeichnung.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Description of Fahrzeug_Trupp_Bezeichnung
- * @author AdminNova 
+ * @author MarcelFeesche 
  */
 
 /*
@@ -20,35 +20,32 @@ $Stützpunkt_2 = $Wachzuordnung[2];
  * Array (vehicle allocation) = Array([Station or SubStation][vehicle 1 - 10][Day and Night or Day or Night]],
 */
 $Fahrzeugzuordnung = [$Dienststelle => ['LHF_1'=>['T/N'], 'DLK'=>['T/N'], 'LHF_2'=>['T/N'], 'RTW_1'=>['T/N]']],
-                      $Stützpunkt_1 => ['RTW_1'=>['T/N'], 'RTW_2'=>['T']],
+                     $Stützpunkt_1 => ['RTW_1'=>['T/N'], 'RTW_2'=>['T']],
                       $Stützpunkt_2 => ['RTW_1'=>['T/N']]];
 
 $arr_length_j = count($Fahrzeugzuordnung);
-print_r($arr_length_j);
+//print_r($arr_length_j);
 
-foreach($Fahrzeugzuordnung as $Ort => $Fahrzeug){
-    str
-    print "Dienststelle: ($Fahrzeug)[] ";
-    Switch($Ort){
+
+foreach($Fahrzeugzuordnung as $Ort){ 
+    echo "Dienststelle:" .$Ort;
+    echo ' ';
+    $Fahrzeuge = array_column($Ort, $arr_length_j);
+            echo $Fahrzeuge;
+           //var_dump($Ort);
+           //var_dump($Fahrzeug);
+    Switch($Fahrzeug){
     case 'LHF_1':
-    Echo"<tr><td class='FunktionAuswahl'>Stf_1<td><td class='FunktionAuswahl'>(37)</td></tr>
+        
+    Echo "<tr><td class='FunktionAuswahl'>Stf_1<td><td class='FunktionAuswahl'>(37)</td></tr>
         <tr><td class='FunktionAuswahl'>MA_1<td><td class='FunktionAuswahl'>(52)</td></tr>
 	<tr><td class='FunktionAuswahl'>Atr_1<td><td class='FunktionAuswahl'>(52)</td></tr>
 	<tr><td class='FunktionAuswahl'>Wtr_1<td><td class='FunktionAuswahl'>(54)</td></tr>
 	<tr><td class='border'><td><td class='border'><td></tr>
     <tr><td class='border'><td><td class='border'><td></tr>";}
-   
 
-//  $arr_length_i = count($Fahrzeugzuordnung);  //max( array_map( 'count', $Fahrzeugzuordnung) );
-//print_r($arr_length_i);
+
 }
 
-for ($j =0; $j < $arr_length_j; $j++) {  
- // for ($i =0; $i < $arr_length_i; $i++) {
-     
-      //print_r($j);
-     // print_r($i);
- 
-    }
 //}
 

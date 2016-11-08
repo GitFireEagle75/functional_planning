2eb4a576bf945d6e6d591af2caa9ef96a0d28495
GitFireEagle75 <feemar2@web.de>
2016-11-08 22:15:59 +0100

    Funktionszuordnung angepasst

--- Datenbanken/newSQLTemplateWachbesetzung.sql
+++ Datenbanken/newSQLTemplateWachbesetzung.sql
@@ -12,7 +12,7 @@ SET IDENTITY_INSERT [dbo].[Dienstgrad] ON;
 
 INSERT INTO [dbo].[Dienstgrad] (Dienstgrad_ID, Dienstgrad) VALUES
 (1, 'LBD'),(2, 'LBDv'),(3, 'BD*'),(4, 'BD'),(5, 'BOR'),(6, 'BR'),(7, 'BRef'),(8, 'BOAR'),(9, 'BAR'),(10, 'BA'),
-(11, 'BOI'),(12, 'BI'),(13, 'BOI_Anw'),(14, 'HBMz'),(15, 'HBM*'),(16, 'HBM'),(17, 'OBM'),(18, 'BM'),(19, 'BM_Anw');
+(11, 'BOI'),(12, 'BI'),(13, 'BOI_Anw'),(14, 'HBMz'),(15, 'HBM*'),(16, 'HBM'),(17, 'OBM'),(18, 'BM'),(19, 'BM_Anw'),(20, 'BM_zA');
 
 SET IDENTITY_INSERT [dbo].[Dienstgrad] OFF;
 
@@ -37,7 +37,7 @@ CREATE TABLE [dbo].[Dienststellen](Dienststellen_ID int PRIMARY KEY,
                     Dienststellenkennung varchar(10),
                     Direktions_ID int,
 
-                    CONSTRAINT Direktions_Fk FOREIGN KEY (Direktions_ID) REFERENCES Direktion(Direktions_ID)); 
+                    CONSTRAINT Direktions_Fk FOREIGN KEY (Direktions_ID) REFERENCES Direktion(Direktions_ID)ON DELETE CASCADE); 
 
 SET IDENTITY_INSERT [dbo].[Dienststellen] ON;
 
@@ -82,8 +82,6 @@ INSERT INTO [dbo].[Stuetzpunkte](Stuetzpunkte_ID, Dienststellenname, Dienststell
 (24, 'RD-SP Marzahn', 6110),(25, 'RD-SP Biesdorf', 6120),(26, 'RD-SP KH Maria Heimsuchung', 2604),(27, 'RD-SP Falkenberg', 6320),(28, 'RD-SP KH Kaulsdorf', 6204),(29, 'RD-SP KH Buch', 2704),
 (30, 'RD-SP Wartenberg/Malchow', 6330),(31, 'RD-SP Hohenschönhausen', 6310);
 
-
-
 SET IDENTITY_INSERT [dbo].[Stuetzpunkte] OFF;
 
 ----------------------------------------------------------------------------------------------------------------------
@@ -124,9 +122,8 @@ INSERT INTO [dbo].[Fahrzeuge](Fahrzeuge_ID, Fahrzeuge_Art, Fahrzeug_Bezeichnung)
 (32, 'FWK', 'FWK-30'),(33, 'TM', 'TM-50'),(34, 'ABC Erkunder', 'ABC Erkunder'),(35, 'Dekon P', 'Dekon P'),(36, 'Dekon G', 'Dekon G'),
 (37, 'GW', 'GW ManV'),(38, 'AB', 'AB ManV'),(39, 'GW', 'GW San'),(40, 'STEMO', 'STEMO'),(41, 'RTW', 'RTW_1'),(42, 'RTW', 'RTW_2'),(43, 'RTW', 'RTW_3'),
 (44, 'RTW', 'RTW_4'),(45, 'RTW', 'RTW_5'),(46, 'RTW', 'RTW_6'),(47, 'RTW', 'RTW_7'),(48, 'RTW', 'RTW_8'),(49, 'RTW', 'RTW_9'),(50, 'RTW', 'RTW_10'),
-(51, 'RTW', 'RTW-S(BTW)'),(52, 'RTW', 'RTW-I(Intensiv)'),(53, 'RTW', 'RTW_ASB'),(54, 'RTW', 'RTW_DRK'),(55, 'RTW', 'RTW_JUH'),(56, 'RTW', 'RTW_MHD'),(57, 'RTW', 'RTW_BW'),(58, 'NEF', 'NEF'),
-(59, 'Fmel', 'Fernmelder'); 
-
+(51, 'RTW', 'RTW-S(BTW)'),(52, 'RTW', 'RTW-I(Intensiv)'),(53, 'RTW', 'RTW_ASB'),(54, 'RTW', 'RTW_DRK'),(55, 'RTW', 'RTW_JUH'),(56, 'RTW', 'RTW_MHD'),
+(57, 'RTW', 'RTW_BW'),(58, 'NEF', 'NEF'),(59, 'Fmel', 'Fernmelder'); 
 
 SET IDENTITY_INSERT [dbo].[Fahrzeuge] OFF;
 
@@ -138,9 +135,9 @@ CREATE TABLE [dbo].[Fahrzeugfunktionen](Fahrzeugfunktionen_ID int PRIMARY KEY,
                     Fahrzeuge_ID int NOT NULL,
                     Funktions_Kennung int NOT NULL,
                     Funktions_Bezeichnung varchar(40),
-                    Grund_Qualifizierung int,
+                    Grund_Qualifizierung int NULL,
 
-                    CONSTRAINT Fahrzeuge_Fk FOREIGN KEY (Fahrzeuge_ID) REFERENCES Fahrzeuge(Fahrzeuge_ID));
+                    CONSTRAINT Fahrzeuge_Fk FOREIGN KEY (Fahrzeuge_ID) REFERENCES Fahrzeuge(Fahrzeuge_ID)ON DELETE CASCADE);
 
 -- Daten für Tabelle "Fahrzeugfunktionen"
 
@@ -169,15 +166,41 @@ INSERT INTO [dbo].[Fahrzeugfunktionen](Fahrzeugfunktionen_ID, Fahrzeuge_ID, Funk
 (45, 1, '543', 'WTr_Pra_Lhf_1', 'NULL'),
 
 (46, 7, '511', 'DLK_Fü', 'NULL'),(47, 7, '512', 'DLK_Ma', 'NULL'),
-(48, 8, '841', 'Klef', 'NULL'),(49, 8, '842', 'Klef', 'NULL'),
+(48, 8, '841', 'Klef_Fü', 'NULL'),(49, 8, '842', 'Klef_Ma', 'NULL'),
 
-(50, 0, '841', 'LKW', 'NULL'), -- Gruppieren aller LKW ?
-(51, 9, '111', 'ELW-A', 'NULL'),(52, 9, '112', 'ELW-A', 'NULL'),
-(53, 10, '141', 'ELW-B', 'NULL'),(54, 10, '142', 'ELW-B', 'NULL'),
-(55, 11, '311', 'ELW-C', 'NULL'),(56, 11, '312', 'ELW-C', 'NULL'),
-(57, 12, '321', 'ELW-C ÄLRD', 'NULL'),(58, 12, '322', 'ELW-C ÄLRD', 'NULL'),
-(59, 13, 'ELW-3', 'ELW-3', 'NULL'),(60, 13, 'ELW-3', 'ELW-3', 'NULL'),
-(61, 14, 'FmeW', 'FmeW', 'NULL'),(62, 19, '571', 'RTB', 'NULL'),(63, 19, '572', 'RTB', 'NULL'),(64, 59, '881', 'FernMel', 'NULL');
+(50, 9, '111', 'ELW-LBD', 'NULL'),(51, 9, '112', 'ELW-LBD_Ma', 'NULL'),
+(52, 9, '121', 'ELW-A_1_Fü', 'NULL'),(53, 9, '122', 'ELW-A_1_Ma', 'NULL'),
+(54, 9, '131', 'ELW-A_2_Fü', 'NULL'),(55, 9, '132', 'ELW-A_2_Ma', 'NULL'),
+
+(56, 10, '141', 'ELW-B_Fü', 'NULL'),(57, 10, '142', 'ELW-B_Ma', 'NULL'),
+(58, 10, '151', 'ELW-B_Fü', 'NULL'),(59, 10, '152', 'ELW-B_Ma', 'NULL'),
+(60, 10, '161', 'ELW-B_Fü', 'NULL'),(61, 10, '162', 'ELW-B_Ma', 'NULL'),
+
+(62, 11, '311', 'ELW-C_Fü', 'NULL'),(63, 11, '312', 'ELW-C_Ma', 'NULL'),
+(64, 12, '321', 'ELW-C ÄLRD_Fü', 'NULL'),(65, 12, '322', 'ELW-C ÄLRD_Ma', 'NULL'),
+--FD
+(66, 11, '591', 'ELW-1-Doku_Fü', 'NULL'),
+(66, 13, '961', 'ELW-3-FL', 'NULL'),
+(66, 13, '962', 'ELW-3_Ma', 'NULL'),(67, 13, '963', 'ELW-3', 'NULL'),
+(61, 14, '', 'FmeW', 'NULL'),(61, 14, '', 'FmeW', 'NULL'),
+--TD
+(61, 14, '991', 'Zfg_TD', 'NULL'),
+(61, 14, '981', 'GW_Mess_Fü', 'NULL'),(61, 14, '982', 'GW_Mess_Ma', 'NULL'),
+(66, 13, '581', 'GW_Wasser_Fü', 'NULL'),(66, 13, '582', 'GW_Wasser_Ma', 'NULL'),
+--LKW
+(61, 14, '571', 'LKW_AB/LB_Fü', 'NULL'),(61, 14, '572', 'LKW_AB/LB_Ma', 'NULL'),(61, 14, '573', 'LKW_AB/LB', 'NULL'),
+(61, , '811', 'LKW_1_Ma', 'NULL'),(61, , '821', 'LKW_2_Ma', 'NULL'),(61, , '831', 'LKW_3_Ma', 'NULL'),
+(61, , '', '', 'NULL'),(61, , '', '', 'NULL'),(61, , '', '', 'NULL'),
+--ABC
+(61, , '341', 'ABC_Erkunder', 'NULL'),(61, , '342', 'ABC_Erkunder', 'NULL'),
+--Klef
+(66, 13, '581', 'Klef', 'NULL'),(66, 13, '582', 'Klef', 'NULL'),
+--GW
+(66, 13, '581', 'GW_San_Fü', 'NULL'),(66, 13, '582', 'Klef', 'NULL'),
+
+(62, 19, '561', 'RTB', 'NULL'),(63, 19, '562', 'RTB', 'NULL'),
+(64, 59, '881', 'FernMel', 'NULL'),(65, 59, '882', 'FernMel', 'NULL'),(66, 59, '883', 'FernMel', 'NULL'),
+(67, 0, '841', 'LKW', 'NULL'); -- Gruppieren aller LKW ?;
 
 SET IDENTITY_INSERT [dbo].[Fahrzeugfunktionen] OFF;
 
@@ -192,8 +215,8 @@ CREATE TABLE [dbo].[Fahrzeugzuordnung_Dienststelle](Dienststellenzuordnung_ID in
                     Dienststellenposition int NOT NULL,
                     Gültig_ab Date NOT NULL,
 
-                    CONSTRAINT Fahrzeuge_Fk FOREIGN KEY (Fahrzeuge_ID) REFERENCES Fahrzeuge(Fahrzeuge_ID),
-                    CONSTRAINT Dienststellen_Fk FOREIGN KEY (Dienststellen_ID) REFERENCES Dienststellen(Dienststellen_ID)) ;
+                    CONSTRAINT Fahrzeuge_Fk FOREIGN KEY (Fahrzeuge_ID) REFERENCES Fahrzeuge(Fahrzeuge_ID)ON DELETE CASCADE,
+                    CONSTRAINT Dienststellen_Fk FOREIGN KEY (Dienststellen_ID) REFERENCES Dienststellen(Dienststellen_ID)ON DELETE CASCADE) ;
 
 SET IDENTITY_INSERT [dbo].[Fahrzeugzuordnung_Dienststelle] ON;
 
@@ -211,8 +234,8 @@ CREATE TABLE [dbo].[Fahrzeugzuordnung_Stützpunkt](Stuetzpunktzuordnung_ID int P
                     Dienststellenposition int,
                     Gültig_ab Date,
 
-                    CONSTRAINT Stuetzpunkt_Fk FOREIGN KEY (Stuetzpunkt_ID) REFERENCES Stuetzpunkt(Stuetzpunkt_ID),
-                    CONSTRAINT Fahrzeuge_Fk FOREIGN KEY (Fahrzeuge_ID) REFERENCES Fahrzeuge(Fahrzeuge_ID));
+                    CONSTRAINT Stuetzpunkt_Fk FOREIGN KEY (Stuetzpunkt_ID) REFERENCES Stuetzpunkt(Stuetzpunkt_ID)ON DELETE CASCADE,
+                    CONSTRAINT Fahrzeuge_Fk FOREIGN KEY (Fahrzeuge_ID) REFERENCES Fahrzeuge(Fahrzeuge_ID)ON DELETE CASCADE);
 
 SET IDENTITY_INSERT [dbo].[Fahrzeugzuordnung_Stützpunkt] ON;
 
@@ -269,15 +292,40 @@ SET IDENTITY_INSERT [dbo].[Funktion_Rettungsdienst] OFF;
 -- Tabellenstruktur für Tabelle "Sonderfunktionen" ? Ermitteln unterschiedlicher Sonderfunktionen ?
 
 CREATE TABLE [dbo].[Sonderfunktionen](Sonderfunktion_ID int PRIMARY KEY,
-                                             Sonderfunktion varchar(15)) ;
+                                        Personal_ID int NOT NULL,
+                                        Maschinist_ELW boolean False,
+                                        Maschinist_RTW boolean False,
+                                        Maschinist_NEF boolean False,
+                                        Maschinist_LKW boolean False,
+                                        Maschinist_DLK boolean False, 
+                                        Maschinist_Kran boolean False,
+                                        Maschinist_TM_50 boolean False,
+                                        Maschinist_Saugwagen boolean False,
+                                        Maschinist_Boot boolean False,
+                                        Maschinist_GW_Mess boolean False,
+                                        Maschinist_GW_Wasser boolean False,
+                                        Maschinist_Dekon_G boolean False,
+                                        Maschinist_Dekon_P boolean False,
+                                        Maschinist_ErKw boolean False,
+                                        Maschinist_GW_San boolean False,
+                                        ABC_Dekon boolean False,
+                                        ABC_Erkunder boolean False,
+                                        Rüstgruppe boolean False,
+                                        Taucher boolean False,
+                                        ANT boolean False,
+                                        ANTS boolean False,
+                                        Langzeitgeräteträger boolean False,
+                                        ELW_3 boolean False,
+
+                        CONSTRAINT Personal_Fk FOREIGN KEY (Personal_ID) REFERENCES Personal(Personal_ID) ON DELETE CASCADE);
 
 -- Daten für Tabelle "Sonderfunktionen"
 
-SET IDENTITY_INSERT [dbo].[Sonderfunktionen] ON ;
+/*SET IDENTITY_INSERT [dbo].[Sonderfunktionen] ON ;
 INSERT INTO [dbo].[Sonderfunktionen] (Sonderfunktion_ID, Sonderfunktionen) VALUES
 (1, ''),(2, ''),(3, ''),(4, ''),(5, ''),(6, ''),(7, 'ohne');
 
-SET IDENTITY_INSERT [dbo].[Sonderfunktionen] OFF;
+SET IDENTITY_INSERT [dbo].[Sonderfunktionen] OFF;*/
 
 ----------------------------------------------------------------------------------------------------------------------
 -- Tabellenstruktur für Tabelle "Wachabteilung"
@@ -305,23 +353,25 @@ CREATE TABLE [dbo].[Funktionsbesetzung](Besetzungs_ID int PRIMARY KEY,
                                         Plan_Dienststelle_Nacht int,
                                         Plan_Funktion_Nacht int,
 
-                                        CONSTRAINT Personal_Fk FOREIGN KEY (Personal_ID) REFERENCES Personal(Personal_ID),
-                                        CONSTRAINT Wachabteilungs_Fk FOREIGN KEY (Wachabteilungs_ID) REFERENCES Wachabteilung(Wachabteilungs_ID)) ;
+                                        CONSTRAINT Personal_Fk FOREIGN KEY (Personal_ID) REFERENCES Personal(Personal_ID)ON DELETE CASCADE,
+                                        CONSTRAINT Wachabteilungs_Fk FOREIGN KEY (Wachabteilungs_ID) REFERENCES Wachabteilung(Wachabteilungs_ID)ON DELETE CASCADE) ;
 
 -----------------------------------------------------------------------------------------------------------------------
--- Tabellenstruktur für Tabelle "Personaldaten"
-CREATE TABLE [dbo].[Personaldaten](Personal_ID int PRIMARY KEY, Adresse varchar(50), Ort varchar(30), PLZ varchar(6),
-                    HNr varchar(10), Zusatz varchar(20), Telefon varchar(25), Handy varchar(25), Geburtsdatum Date);
+-- Tabellenstruktur für Tabelle "Personaldaten" 
+CREATE TABLE [dbo].[Personaldaten](Personal_ID int PRIMARY KEY,
+                                    Telefon varchar(25), 
+                                    Handy varchar(25), 
+                                    Geburtsdatum Date);
 
 SET IDENTITY_INSERT [dbo].[Personaldaten] ON;
 
---INSERT INTO [dbo].[Personaldaten](Personal_ID int, Adresse, Ort, PLZ, HNr, Zusatz, Telefon, Handy, Geburtsdatum) VALUES
+--INSERT INTO [dbo].[Personaldaten](Personal_ID int, Telefon, Handy, Geburtsdatum) VALUES
 
 SET IDENTITY_INSERT [dbo].[Personaldaten] OFF;
 ----------------------------------------------------------------------------------------------------------------------
 -- Tabellenstruktur für Tabelle "Profilbilder"
 CREATE TABLE [dbo].[Profilbilder](Profilbild_ID int PRIMARY KEY,
-                                  Dateiname varchar(8),
+                                  Dateiname varchar(8), --Personalnummer
                                   ImageData image) ;
 
 SET IDENTITY_INSERT [dbo].[Profilbilder] ON;
@@ -337,9 +387,13 @@ CREATE TABLE [dbo].[Stützpunkte_has_Dienststellen](Dienststelle_has_ID int PRIM
                                                     Stuetzpunkt_ID int,
                                                     Stuetzpunkt_Rang int,
 
-                                                    CONSTRAINT StuePun_Fk FOREIGN KEY (Stuetzpunkt_ID) REFERENCES Dienstgrad(Dienstgrad_ID),
-                                                    CONSTRAINT StuePunRang_Fk FOREIGN KEY (Stuetzpunkt_ID) REFERENCES Dienstgrad(Dienstgrad_ID));
+                                                    CONSTRAINT StuePun_Fk FOREIGN KEY (Stuetzpunkt_ID) REFERENCES Dienstgrad(Dienstgrad_ID)ON DELETE CASCADE,
+                                                    CONSTRAINT StuePunRang_Fk FOREIGN KEY (Stuetzpunkt_ID) REFERENCES Dienstgrad(Dienstgrad_ID)ON DELETE CASCADE);
+SET IDENTITY_INSERT [dbo].[Stützpunkte_has_Dienststellen] ON;
 
+--INSERT INTO [dbo].[Profilbilder](Dienststelle_has_ID, Stuetzpunkt_ID , Stuetzpunkt_Rang) VALUES
+
+SET IDENTITY_INSERT [dbo].[Stützpunkte_has_Dienststellen] OFF;
 ----------------------------------------------------------------------------------------------------------------------
 -- Tabellenstruktur für Tabelle "Wachzuordnung"
 CREATE TABLE [dbo].[Wachzuordnung](Wachzuordnung_ID int PRIMARY KEY) ;
@@ -362,10 +416,10 @@ CREATE TABLE [dbo].[Personal](Personal_ID int PRIMARY KEY,
             Dienststellen_ID int,
             Wachabteilung_ID int,
             FwZuordnung_ID int
-            CONSTRAINT DG_Fk FOREIGN KEY (Dienstgrad_ID) REFERENCES Dienstgrad(Dienstgrad_ID),
-            CONSTRAINT Dienststellen_Fk FOREIGN KEY (Dienststellen_ID)  REFERENCES Dienststellen(Dienststellen_ID),
-            CONSTRAINT Wachabteilung_Fk FOREIGN KEY (Wachabteilung_ID)  REFERENCES Wachabteilung(Wachabteilung_ID),
-            CONSTRAINT FwZuordnung_Fk FOREIGN KEY (FwZuordnung_ID)  REFERENCES FwZuordnung(FwZuordnung_ID));
+            CONSTRAINT DG_Fk FOREIGN KEY (Dienstgrad_ID) REFERENCES Dienstgrad(Dienstgrad_ID)ON DELETE CASCADE,
+            CONSTRAINT Dienststellen_Fk FOREIGN KEY (Dienststellen_ID)  REFERENCES Dienststellen(Dienststellen_ID)ON DELETE CASCADE,
+            CONSTRAINT Wachabteilung_Fk FOREIGN KEY (Wachabteilung_ID)  REFERENCES Wachabteilung(Wachabteilung_ID)ON DELETE CASCADE,
+            CONSTRAINT FwZuordnung_Fk FOREIGN KEY (FwZuordnung_ID)  REFERENCES FwZuordnung(FwZuordnung_ID)ON DELETE CASCADE);
 
 SET IDENTITY_INSERT [dbo].[Personal] ON;
 
